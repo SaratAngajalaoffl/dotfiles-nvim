f4e4facf37b78292e0f5b3af0d667cd68e9f5ebb
mugiwara@thor <saratangajala@gmail.com>
2026-05-30 20:33:44 +0530

    Enable snacks.picker as fuzzy finder

--- config/lua/plugins/snacks.lua
+++ config/lua/plugins/snacks.lua
@@ -7,12 +7,22 @@ return {
     indent = { enabled = true },
     lazygit = { enabled = true },
     notifier = { enabled = true },
+    picker = { enabled = true },
     statuscolumn = { enabled = true },
     terminal = { enabled = true },
   },
   keys = {
+    -- Find
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find files" },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Live grep" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+    { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent files" },
+    { "<leader>fh", function() Snacks.picker.help() end, desc = "Help pages" },
+    { "<leader>/",  function() Snacks.picker.grep_buffers() end, desc = "Search open buffers" },
+    -- Git
     { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
     { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Git log (file)" },
     { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Git log" },
+    { "<leader>gc", function() Snacks.picker.git_log() end, desc = "Git commits" },
   },
 }
